90a5621f3459935ffe797147b47ba1086fe8b261
Sally Lee <sally.orkio@gmail.com>
2020-02-16 22:29:03 +0100

    Feat: Login
    * Change default route to ~/Account/Login
    * If login suceed, go to ~/MoviesRental/Genres
    * If login failed, go to LoginError view
    * Add /Services/MoviesRentalDbService to connect db
    * Add  LoginViewModel in /Model/AccountViewModels
    * Add link to logo image in _Layout view
    * Enable client -side validation

---
+++ FooMoviesRental/Services/MoviesRentalDbService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FooMoviesRental.Models;
+
+namespace FooMoviesRental.Services
+{
+    public class MoviesRentalDbService
+    {
+        public Klant GetKlant(string naam, int? postcode)
+        {
+            using (var db = new FooMoviesRentalEntities())
+            {
+                var query = from k in db.Klanten
+                            where k.Naam == naam && k.PostCode == postcode
+                            select k;
+                return query.FirstOrDefault();
+            }
+        }
+
+    }
+}
