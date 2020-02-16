93179d4803f324dee050c2cb23288d844fdeff97
Sally Lee <sally.orkio@gmail.com>
2020-02-17 00:05:36 +0100

    Fix: Add  euro sign (€) before price in the list of films
    * Add DisplayFormat attribute to Prijs property to define the format of Prijs

---
+++ FooMoviesRental/Models/FilmProperties.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace FooMoviesRental.Models
+{
+    public class FilmProperties
+    {
+        [DisplayFormat(DataFormatString = "€ {0:#,##0.00}")]
+        public decimal Prijs { get; set; }
+    }
+}
